23b9f4fea717b0a3eda7381abd4bd61eae76f79b
Rishal Singh <65354251+Rishal-Singh@users.noreply.github.com>
2026-05-25 01:27:39 +0530

    Update sonar.sh

--- sonar.sh
+++ sonar.sh
@@ -12,3 +12,53 @@ su - sonar
 #run this on server manually
 #sh /opt/sonarqube-8.9.6.50800/bin/linux/sonar.sh start
 #echo "user=admin & password=admin"
+
+=========================================================== installing Sonarqube using Dockerhub ==============================================
+
+
+sudo apt update && sudo apt upgrade -y
+
+sudo systemctl enable docker
+sudo systemctl start docker
+
+docker --version
+
+Fix Docker Permission
+
+sudo usermod -aG docker $USER
+newgrp docker
+
+docker ps
+
+sudo sysctl -w vm.max_map_count=524288
+sudo sysctl -w fs.file-max=131072
+
+sudo nano /etc/sysctl.conf
+
+vm.max_map_count=524288
+fs.file-max=131072
+
+sudo sysctl -p
+
+docker pull sonarqube:lts-community
+
+docker pull sonarqube:latest
+
+docker run -d \
+  --name sonarqube \
+  -p 9000:9000 \
+  sonarqube:lts-community
+
+  docker run -d \
+  --name sonarqube \
+  -p 9000:9000 \
+  -v sonarqube_data:/opt/sonarqube/data \
+  -v sonarqube_logs:/opt/sonarqube/logs \
+  -v sonarqube_extensions:/opt/sonarqube/extensions \
+  sonarqube:lts-community
+
+  docker ps
+
+  docker logs -f sonarqube
+
+  
