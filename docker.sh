fdcc109de0348b0d736cbe8e94d85458618011ce
Rishal Singh <65354251+Rishal-Singh@users.noreply.github.com>
2026-05-26 14:30:43 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -3,13 +3,14 @@
 # Run the following command to uninstall all conflicting packages:
 sudo apt remove $(dpkg --get-selections docker.io docker-compose docker-compose-v2 docker-doc podman-docker containerd runc | cut -f1)
 
+
+#run all at once
 # Add Docker's official GPG key:
 sudo apt update
 sudo apt install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-
 # Add the repository to Apt sources:
 sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
@@ -25,6 +26,8 @@ sudo apt update
 
 # To install the latest version, run:
 sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+#install docker compose also
 
 sudo systemctl status docker
 sudo systemctl start docker
