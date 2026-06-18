939d60b0163f637391f7e653479bab2e607d862f
Rishal Singh <65354251+Rishal-Singh@users.noreply.github.com>
2026-06-18 15:46:54 +0530

    Update minikube.sh

--- minikube.sh
+++ minikube.sh
@@ -1,14 +1,51 @@
+Minikube setup 
+=========================================================================================
+
+3 Requirement : Docker, Kubectl and Minikube.
+=========================================================================================
+
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install curl wget apt-transport-https -y
-sudo curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo mv minikube-linux-amd64 /usr/local/bin/minikube
-sudo chmod +x /usr/local/bin/minikube
-sudo minikube version
-sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
-sudo echo "$(cat kubectl.sha256) kubectl" | sha256sum --check
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-sudo minikube start --driver=docker --force
+
+Docker:
+
+sudo apt install docker.io -y
+
+sudo systemctl enable docker
+sudo systemctl start docker
+
+docker --version
+
+sudo usermod -aG docker $USER
+newgrp docker
+
+docker ps
+
+Kubectl :
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+chmod +x kubectl
+
+sudo mv kubectl /usr/local/bin/
+
+kubectl version --client
+
+Minikube:
+
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+minikube version
+
+minikube start --driver=docker
+
+minikube status
+kubectl get nodes
+
+addons minikube:
+minikube addons enable metrics-server
+minikube addons enable ingress
+minikube addons enable dashboard
